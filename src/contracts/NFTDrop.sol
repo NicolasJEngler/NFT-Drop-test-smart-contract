ea9ef83f28f6da1b4736c4c49e6a4ecc023fd51f
Nicolás J. Engler <nicojengler@gmail.com>
2022-02-10 06:54:58 -0300

    Initial commit
    Squashed commit of the following:
    
    commit 3cb61ebbe60b3e1f6be9c5819b9abcf8eb019132
    Author: Nicolás J. Engler <nicojengler@gmail.com>
    Date:   Wed Feb 9 15:27:51 2022 -0300
    
        Update smart contract
    
    commit 22fb3ec0810eb98f52eb29e40c3dc4e4625968bb
    Merge: 75d1f66 fdab197
    Author: monkjuice <agustinbarg+1@gmail.com>
    Date:   Tue Dec 14 08:36:45 2021 -0300
    
        Merge pull request #2 from REDACTED/develop
    
        Develop
    
    commit 75d1f661e470161520d3e3f53fdd48b7067ce7aa
    Merge: 04d3632 5c4c2ef
    Author: Nicolás J. Engler <nicojengler@gmail.com>
    Date:   Mon Dec 13 21:48:59 2021 -0300
    
        Merge pull request #1 from REDACTED/update/cid-legals-capitalization
    
        Update legal disclaimer. Update CID. Correct capitalization.
    
    commit 5c4c2eff5b0c91f49d51c52d017c5e1e2fc9ec46
    Author: monkjuice <agustinbarg+1@gmail.com>
    Date:   Mon Dec 13 20:51:39 2021 -0300
    
        Update REDACTED.test.ts
    
    commit a8a5d94469eb00aabf07148525703c2e561b08ac
    Merge: fb0dde0 04d3632
    Author: Nicolás J. Engler <nicojengler@gmail.com>
    Date:   Mon Dec 13 20:01:36 2021 -0300
    
        Merge branch 'main' into update/cid-legals-capitalization
    
    commit fb0dde0e53f92968f25e17e7cbaff545c74a1e92
    Author: Nicolás J. Engler <nicojengler@gmail.com>
    Date:   Mon Dec 13 20:00:01 2021 -0300
    
        Update legal disclaimer. Update CID. Correct capitalization.
    
    commit fdab197c55f846b5b82864022e987deb3be68894
    Author: unknown <agusbarg@hotmail.com>
    Date:   Mon Dec 13 17:09:18 2021 -0300
    
        fix conflicting import
    
    commit ee4fcd1bf5196e59b8a6daf7bf84d711f582d151
    Author: unknown <agusbarg@hotmail.com>
    Date:   Mon Dec 13 16:05:27 2021 -0300
    
        remove lock file
    
    commit 01dea86434abe1a17ef95e3526e21d462d598f06
    Author: unknown <agusbarg@hotmail.com>
    Date:   Mon Dec 13 15:59:36 2021 -0300
    
        update readme
    
    commit 8186b281b490025d3c82ca056c9c3e4c24d935c3
    Author: unknown <agusbarg@hotmail.com>
    Date:   Mon Dec 13 15:55:28 2021 -0300
    
        add hardhat etherscan verify plugin
    
    commit 04d363249831c0de2fac88c62c82b25838a85c7b
    Author: unknown <agusbarg@hotmail.com>
    Date:   Wed Dec 8 15:36:03 2021 -0300
    
        remove baseURI setter from constructor.. should only be set through setBaseURI method now
    
    commit d7e73f8980f5de06bb77906aaa595808b807515a
    Author: unknown <agusbarg@hotmail.com>
    Date:   Wed Dec 8 10:00:55 2021 -0300
    
        first commit
    
    commit 136517bfe2ee8be9e6dcd22a3490be1e235cef61
    Author: monkjuice <agustinbarg+1@gmail.com>
    Date:   Tue Dec 7 20:20:43 2021 -0300
    
        Initial commit

---
+++ src/contracts/NFTDrop.sol
@@ -0,0 +1,87 @@
+/***
+NFT Drop Test Smart Contract
+***/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract NFTDrop is ERC721Enumerable, Ownable {
+    using MerkleProof for bytes32[];
+
+    mapping(address => bool) public alreadyMinted;
+
+    uint16 private reserveNFTsId;
+    uint16 private NFTsId;
+
+    bytes32 public merkleRoot;
+    bool public merkleEnabled = true;
+
+    string private baseURI;
+
+    bool private saleStarted = true;
+    uint256 public constant maxMint = 2222;
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    constructor() ERC721("NFT Drop", "NFTDrop") {
+        baseURI = "";
+        reserveNFTsId = 1; // item 1-50
+        NFTsId = 51; // item 51-2222
+    }
+
+    function setBaseURI(string memory _baseUri) public onlyOwner {
+        baseURI = _baseUri;
+    }
+
+    function mint(bytes32[] memory proof, bytes32 leaf) public returns (uint256) {
+        // merkle tree
+        if (merkleEnabled) {
+            require(keccak256(abi.encodePacked(msg.sender)) == leaf, "This leaf does not belong to the sender");
+            require(proof.verify(merkleRoot, leaf), "You are not in the list");
+        }
+
+        require(saleStarted == true, "The sale is paused");
+        require(msg.sender != address(0x0), "Public address is not correct");
+        require(alreadyMinted[msg.sender] == false, "Address already used");
+        require(NFTsId <= maxMint, "Mint limit reached");
+
+        _safeMint(msg.sender, NFTsId++);
+
+        alreadyMinted[msg.sender] = true;
+
+        return NFTsId;
+    }
+
+    function reserveNFTs(address to, uint8 amount) public onlyOwner {
+        require(reserveNFTsId + amount <= 51, "Out of stock");
+
+        for (uint8 i = 0; i < amount; i++) _safeMint(to, reserveNFTsId++);
+    }
+
+    function startSale() public onlyOwner {
+        saleStarted = true;
+    }
+
+    function pauseSale() public onlyOwner {
+        saleStarted = false;
+    }
+
+    function setMerkleRoot(bytes32 _root) public onlyOwner {
+        merkleRoot = _root;
+    }
+
+    function startMerkle() public onlyOwner {
+        merkleEnabled = true;
+    }
+
+    function stopMerkle() public onlyOwner {
+        merkleEnabled = false;
+    }
+}
---
+++ test/testNFTDrop.test.ts
@@ -0,0 +1,100 @@
+import {expect} from './chai-setup';
+import {ethers, deployments, getUnnamedAccounts} from 'hardhat';
+import {setupUsers} from './utils';
+import {NFTDrop} from '../typechain/NFTDrop';
+import {Bytes} from '@ethersproject/bytes';
+import {MerkleTree} from 'merkletreejs';
+import {keccak256, toBuffer} from 'ethereumjs-util';
+
+const setup = deployments.createFixture(async () => {
+  await deployments.fixture('NFTDrop');
+
+  const contracts = {
+    NFTDrop: <NFTDrop>await ethers.getContract('NFTDrop'),
+  };
+
+  const users = await setupUsers(await getUnnamedAccounts(), contracts);
+
+  const leaves = users.slice(0, 4).map((x) => keccak256(toBuffer(x.address)));
+
+  const tree = new MerkleTree(leaves, keccak256, {
+    sortLeaves: true,
+    sortPairs: true,
+  });
+
+  contracts.NFTDrop.setMerkleRoot(tree.getRoot());
+
+  return {
+    ...contracts,
+    users,
+    tree,
+  };
+});
+
+const emptyBytes32Array: Bytes[] = [];
+const dummyBytes32 =
+  '0x0000000000000000000000000000000000000000000000000000000000000001';
+
+describe('NFTDrop', function () {
+  it('can mint with merkleroot', async function () {
+    const {users, NFTDrop, tree} = await setup();
+
+    const leaf = '0x' + keccak256(toBuffer(users[0].address)).toString('hex');
+    const proof = tree.getHexProof(leaf);
+
+    await expect(users[0].NFTDrop.mint(proof, leaf))
+      .to.emit(NFTDrop, 'Transfer')
+      .withArgs(
+        '0x0000000000000000000000000000000000000000',
+        users[0].address,
+        51
+      );
+  });
+  it('should fail merkleproof verification', async function () {
+    const {users, tree} = await setup();
+
+    const leaf = '0x' + keccak256(toBuffer(users[5].address)).toString('hex');
+    const proof = tree.getHexProof(leaf);
+
+    await expect(users[0].NFTDrop.mint(proof, leaf)).to.be.revertedWith(
+      'This leaf does not belong to the sender'
+    );
+  });
+  it('can mint without merkleroot', async function () {
+    const {users, NFTDrop} = await setup();
+
+    await NFTDrop.stopMerkle();
+
+    await expect(users[0].NFTDrop.mint(emptyBytes32Array, dummyBytes32))
+      .to.emit(NFTDrop, 'Transfer')
+      .withArgs(
+        '0x0000000000000000000000000000000000000000',
+        users[0].address,
+        51
+      );
+  });
+  it('should mint entire collection', async function () {
+    const {users, NFTDrop} = await setup();
+
+    // avoid testing this if collection has over 60 items due to unnamed accounts length
+    if ((await NFTDrop.maxMint()).toNumber() < 61) {
+      await NFTDrop.stopMerkle();
+
+      await NFTDrop.reserveNFTs(users[0].address, 25);
+      await NFTDrop.reserveNFTs(users[0].address, 25);
+
+      for (let i = 0; i < 10; i++)
+        await expect(
+          users[i].NFTDrop.mint(emptyBytes32Array, dummyBytes32)
+        )
+          .to.emit(NFTDrop, 'Transfer')
+          .withArgs(
+            '0x0000000000000000000000000000000000000000',
+            users[i].address,
+            51 + i
+          );
+
+      await expect(await NFTDrop.totalSupply()).to.be.equal('60');
+    }
+  });
+});
